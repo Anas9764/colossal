99edf75d0ce41a9f6640e51e17597ca1a83dabe0
Anas9764 <s19_qureshi_anas@mgmcen.ac.in>
2023-03-18 18:41:30 +0530

    navbar or bg video done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react';
+import Hero from './components/Herosection/Hero';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Hero/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,42 @@
+// import React ,{ useRef }from 'react'
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./Navbar.css"
+import logo from '../../assets/images/logo.PNG';
+import polylogo from '../../assets/images/polylogo.png'
+const Navbar = () => {
+    const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
+  return (
+    <header>
+    <divc className="head-logo">
+    <img id="logo1"  src={polylogo} alt="" />
+    <img id="logo2" src={logo} alt="" />
+    </divc>
+    {/* <h3>LOGO</h3> */}
+    <nav ref={navRef}>
+        <a href="/#">Home</a>
+        <a href="/#">My work</a>
+        <a href="/#">Blog</a>
+        <a href="/#">About me</a>
+        <button
+            className="nav-btn nav-close-btn"
+            onClick={showNavbar}>
+            <FaTimes />
+        </button>
+    </nav>
+    <button
+        className="nav-btn"
+        onClick={showNavbar}>
+        <FaBars />
+    </button>
+</header>
+  )
+}
+
+export default Navbar
